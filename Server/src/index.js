fa49bc346d44aee4655b0bd8567847ae9e27a1ce
SebastianMartel <martel.ssebastian@gmail.com>
2023-05-17 12:33:53 -0500

    axios installed, controllers added

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,17 +1,16 @@
 const http = require("http")
-const data = require('./utils/data')
+const { getChardById } = require ('./controllers/getCharById')
 
 const PORT = 3001
 
 http
 .createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    
     if (req.url.includes('/rickandmorty/character')) {
         const id = req.url.split('/').at(-1)
-        const characterFound = data.find((char) => char.id === -id)
 
-        return res.writeHead(200, {'Content-type' : 'application/json'})
-        .end(JSON.stringify(characterFound))
+        getChardById(res, +id) 
     }
 })
-.listen(PORT) //works with '0.0.0.0' instead of 'localhost'
+.listen(PORT, 'localhost') //works with '0.0.0.0' instead of 'localhost'
